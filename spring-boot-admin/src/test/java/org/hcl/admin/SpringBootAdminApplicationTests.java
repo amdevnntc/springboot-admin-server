a5ec52e2516fa78c40e2f6a76f5971771b7f6247
dev <dev@dev-PC>
2020-04-30 00:31:26 +0530

    final commit

---
+++ spring-boot-admin/src/test/java/org/hcl/admin/SpringBootAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hcl.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
